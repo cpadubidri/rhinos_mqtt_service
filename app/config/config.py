214cc937c989ffb4a7b9d81ab527cf6e6e1c293e
cpadubidri <chirag.padubidri@gmail.com>
2025-04-06 20:41:53 +0300

    Add topfly message type with code

--- app/config/config.py
+++ app/config/config.py
@@ -1,3 +1,11 @@
 MQTT_BROKER = "94.46.212.151"
 MQTT_PORT = 4444
 TIMEOUT_DURATION = 30  # in seconds
+MESSAGE_TYPES = {
+    "252501": "Login Message",
+    "252502": "Heartbeat Message",
+    "252510": "BLE Message",
+    "252513": "Position Message",
+    "252514": "Alarm Message"
+}
+
